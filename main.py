f0f2943a297182f20d5cca6c9a9541ca10e80461
Jasper Hoogveld <jasper@hoogveld.org>
2022-08-16 11:23:13 +0200

    Works but writes only to the buy_csv

--- main.py
+++ main.py
@@ -32,21 +32,22 @@ def main():
     group1.add_argument("sell", help="requires -prod and -exp", nargs='?')
     group1.add_argument("report", help="print report of revenue or profit", nargs='?')
     parser.add_argument("-prod", help="enter a product to buy or sell", type=str, nargs='?')
+    parser.add_argument("-amount", help="used with buy|sell options", type=int, nargs='?')
     parser.add_argument("-price", help="used with buy|sell options", type=int, nargs='?')
     parser.add_argument("-exp", help="used with buy|sell options", nargs='?') # , type=date fails ??
     group2.add_argument("-inventory", help="show inventory (used with report option)", nargs='?')
     group2.add_argument("-revenue", help="report revenue (used with report option)", nargs='?')
     group2.add_argument("-profit", help="report profit (used with report option)", nargs='?')
     parser.add_argument("-set-time", help="set different date for processing", type=int)
-    group3.add_argument("-yesterday", help="show yesterday's inventory|revenue|profit")
-    group3.add_argument("-now", help="show current inventory|revenue|profit")
+    group3.add_argument("--yesterday", help="show yesterday's inventory|revenue|profit")
+    group3.add_argument("--now", help="show current inventory|revenue|profit")
     args = parser.parse_args()    
 
     if args.buy:
-        csv_writer(buy_csv, args.prod, args.price, args.exp)
+        csv_writer(buy_csv, args.prod, args.amount, args.price, args.exp)
         
     if args.sell:
-        csv_writer(sell_csv, args.prod, args.price, args.exp)
+        csv_writer(sell_csv, args.prod, args.amount, args.price, args.exp)
 
 
 # def BuySell():
@@ -77,13 +78,13 @@ def main():
 buy_csv = os.path.join(sys.path[0], 'bought.csv')
 sell_csv = os.path.join(sys.path[0], 'sold.csv')
 
-def csv_writer(csv_file, prod, price, exp):
+def csv_writer(csv_file, prod, amnt, price, exp):
     ## If file doesn't exist, create it with the correct headers
     if not os.path.exists(csv_file):
         file = Path(csv_file)
         file.touch()
         # Write header
-        headerlist = ['ID', 'Product', 'BuyPrice', 'Exp-Date']
+        headerlist = ['ID', 'Product', 'Amount', 'BuyPrice', 'Exp-Date']
         with open (csv_file, 'w', newline='') as open_csv:
             head = csv.DictWriter(open_csv, delimiter=',', fieldnames = headerlist)
             head.writeheader()
@@ -102,7 +103,7 @@ def csv_writer(csv_file, prod, price, exp):
     # Add product as row to csv
     with open (csv_file, 'a', newline='') as open_csv:
         writer = csv.writer(open_csv)
-        new_row = [int(last_used_ID)+1, prod, price, exp]
+        new_row = [int(last_used_ID)+1, prod, amnt, price, exp]
         writer.writerow(new_row)
         # input("Seems to get to this point")
     return csv_file
