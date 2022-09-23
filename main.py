8ba3728bf357b7dc5e1552d02629919e2103d4cd
Jasper Hoogveld <jasper@hoogveld.org>
2022-09-23 13:45:03 +0200

    Added advance_date

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ __human_name__ = "superpy"
 today = datetime.now()
 stoday = today.strftime("%Y-%m-%d")
 smonth = today.strftime("%Y-%m")
-
+adv_date = today
 
 def main():
     parser = argparse.ArgumentParser(prog="Inventory Manager",
@@ -42,9 +42,8 @@ def main():
 
     report_parser = subparsers.add_parser('report', help='Produce a report')
     report_parser.add_argument("-mode", choices=['inventory', 'revenue', 'profit'])
-    # report_parser.add_argument("-inventory", help="show inventory (used with report option)", nargs='?')
-    # report_parser.add_argument("-revenue", help="report revenue (used with report option)", nargs='?')
-    # report_parser.add_argument("-profit", help="report profit (used with report option)", nargs='?')
+
+    parser.add_argument('adv_date', help="type a number of days you want to check inventory in the future")
 
     args = parser.parse_args()
 
@@ -60,8 +59,16 @@ def main():
         elif args.mode == 'revenue':
             get_revenue(args.spec_date)
         elif args.mode == 'profit':
-            get_profit(args.spec_date)      
+            get_profit(args.spec_date)  
 
+    if args.command == 'adv_date':
+        advance_date(args.delta)
+
+
+# Files locations
+buy_csv = os.path.join(sys.path[0], 'bought.csv')
+sell_csv = os.path.join(sys.path[0], 'sold.csv')
+advanced_date = os.path.join(sys.path[0], 'date.txt')
 
 class Product():
     def __init__(self, prod_name, price, exp_date):
@@ -69,32 +76,38 @@ class Product():
         self.price = price
         self.exp_date = exp_date
 
-
-def advance_time(adv_time):
-    # Create txt file with current dat in YYYY-MM-DD
-    current_date = os.path.join(sys.path[0], 'date.txt')
-    if not os.path.exists(current_date):
-        file = Path(current_date)
+def advance_date(adv_delta):
+    # Create txt file with advanced date in YYYY-MM-DD
+    adv_date = datetime.strftime(today + timedelta(days=adv_delta), '%Y-%m-%d')
+    #advanced_date = os.path.join(sys.path[0], 'date.txt')
+    if not os.path.exists(advanced_date):
+        file = Path(advanced_date)
         file.touch()
-    with open(current_date, 'w') as file:
-        file.write(stoday)
+    with open(advanced_date, 'w') as file:
+        file.write(adv_date)
 
     # Return a date with the requested delta
-    req_time = datetime.strftime(today + timedelta(days=adv_time), '%Y-%m-%d')
-    return req_time
+    return (f' Date for processing set to {adv_date}')
 
 
 def get_inventory(buy_csv, sell_csv):
     bought_list = []
     sold_list = []
 
+    # Read advanced date if set
+    if exists(advanced_date):
+        with open(advanced_date, 'r') as f:
+            adv_date = f.readline()
+        adv_date = datetime.strptime(adv_date, '%Y-%m-%d')
+    else:
+        adv_date = today
     # Add all rows from buy_csv to a bought_list
     with open(buy_csv, 'r', newline='') as open_csv:
         in_file = csv.DictReader(open_csv)
         for row in in_file:
             row['in_inv'] = row['Amount']
             exp_date = datetime.strptime(row['Exp_Date'], '%Y-%m-%d')
-            if exp_date > today:
+            if exp_date > adv_date:
                 row['is_expired'] = 0
             else:
                 row['is_expired'] = 1
@@ -107,26 +120,29 @@ def get_inventory(buy_csv, sell_csv):
             sold_list.append(row)
 
     # Subtract all sold amounts from 'in_inv' column in bought_list
-    for key in sold_list:
-        sold_prod = key['Product']
-        sold_amnt = int(key['Amount'])
-        bought_ID = key['Bought_ID']
-        for buy_key in bought_list:
-            a = buy_key['Product']
-            if sold_prod == buy_key['Product'] and bought_ID == buy_key['ID']:
-                in_inv = int(buy_key['in_inv'])
+    for item in sold_list:
+        sold_prod = item['Product']
+        sold_amnt = int(item['Amount'])
+        bought_ID = item['Bought_ID']
+        for item in bought_list:
+            if sold_prod == item['Product'] and bought_ID == item['ID']:
+                in_inv = int(item['in_inv'])
                 if in_inv == 0:
                     continue
                 elif in_inv > sold_amnt:
-                    buy_key['in_inv'] = in_inv - sold_amnt
+                    item['in_inv'] = in_inv - sold_amnt
                     break
                 else:
-                    buy_key['in_inv'] = 0
+                    item['in_inv'] = 0
                     break
             else:
                 continue
-
-    return bought_list
+    
+    # Delete avanced date file
+    if exists(advanced_date):
+        os.remove(advanced_date)
+           
+    return print(bought_list)
 
 
 def get_revenue(spec_date):
@@ -137,16 +153,14 @@ def get_revenue(spec_date):
         for row in in_file:
             sold_list.append(row)
 
-    # Check if Day, Month or Year and add totals of sales prices for that period per sold item
-    timespan = len(spec_date)
-    for key in sold_list:
-        total_revenue = 0
-        sell_price = key['Sell_Price']
-        sell_date = key['Sell_Date'][:timespan:]
-        if spec_date == sell_date:
+    # Check if Day, Month or Year and add totals of sales prices for that period
+    total_revenue = 0
+    for item in sold_list:
+        sell_price = item['Sell_Price']
+        if item['Sell_Date'].startswith(spec_date):
             total_revenue = total_revenue + float(sell_price)
 
-    return (f'The total revenue for {spec_date} is {total_revenue} euros')
+    return print(f'The total revenue for {spec_date} is {total_revenue} euros')
 
 
 def get_profit(spec_date):
@@ -163,26 +177,23 @@ def get_profit(spec_date):
         for row in in_file:
             bought_list.append(row)
 
-    # Add total sold
-    timespan = len(spec_date)
+    # Add total sold for that period
     total_sold = 0
-    for key in sold_list:
-        sell_price = key['Sell_Price']
-        sell_date = key['Sell_Date'][:timespan:]
-        if spec_date == sell_date:
+    for item in sold_list:
+        sell_price = item['Sell_Price']
+        if item['Sell_Date'].startswith(spec_date):
             total_sold = total_sold + int(sell_price) 
 
-    # Add total bought
+    # Add total bought for that period
     total_bought = 0
-    for key in bought_list:
-        buy_price = key['Buy_Price']
-        buy_date = key['Buy_Date'][:timespan:]
-        if spec_date == buy_date:
+    for item in bought_list:
+        buy_price = item['Buy_Price']
+        if item['Buy_Date'].startswith(spec_date):
             total_bought = total_bought + int(buy_price)
     
     total_profit = float(total_sold) - float(total_bought)
 
-    return (f'The total profit for {spec_date} is {total_profit} euros')    
+    return print(f'The total profit for {spec_date} is {total_profit} euros')    
 
 # CSV Files locations
 buy_csv = os.path.join(sys.path[0], 'bought.csv')
@@ -274,9 +285,9 @@ def sell_csv_writer(sell_csv_file, prod, sell_date, amnt, price):
 #csv = sell_csv_writer(sell_csv, 'banana', stoday, 3 , 12)
 #csv = buy_csv_writer(buy_csv, 'banana', stoday, 2 , 12, stoday)
 #print(get_inventory(buy_csv, sell_csv))
-#print(advance_time(current_date, 2))
-#print(get_revenue('2022-09-22'))
-print(get_profit('2022'))
+#print(advance_date(60))
+#print(get_revenue())
+#print(get_profit('2022'))
 
 if __name__ == "__main__":
     main()
